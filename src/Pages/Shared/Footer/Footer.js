787f940357fdff384f659d2fb9de618a6597631a
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-30 20:29:13 +0000

    header, footer and banner created

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,7 @@ import logo from '../../../assets/logo.svg'
 const Footer = () => {
     return (
         <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+            <footer className="footer p-24 bg-black text-white">
                 <div>
                     <img src={logo} alt="" />
                     <p>Cool Cars Ltd.<br />A complete car solution since 2018</p>
