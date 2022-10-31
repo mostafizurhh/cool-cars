5717ea8541891708ad2a293989f4b16e88b47691
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-31 13:12:38 +0000

    about section done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Banner.css'
 import img1 from '../../../assets/images/banner/1.jpg'
 import img2 from '../../../assets/images/banner/2.jpg'
 import img3 from '../../../assets/images/banner/3.jpg'
--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './BannerItem.css'
 
 const BannerItem = ({ slide }) => {
     const { id, image, prev, next } = slide
     return (
         <div id={`slide${id}`} className="carousel-item relative w-full">
             <div className='gradient'>
-                <img src={image} alt='' className="w-full" />
+                <img src={image} alt='' className="w-full rounded-lg" />
             </div>
             <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/4">
                 <h5 className='text-6xl text-white font-bold'>Affordable <br />
@@ -16,8 +17,8 @@ const BannerItem = ({ slide }) => {
                 <p className='text-white text-xl w-2/3'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
             </div>
             <div className="absolute flex justify-start transform -translate-y-1/2 left-24 bottom-1/3">
-                <button className="btn btn-sm btn-error mr-5">Error</button>
-                <button className="btn btn-sm btn-ghost btn-outline btn-secondary">Button</button>
+                <button className="btn btn-sm bg-orange-700 mr-5 text-whiter">Discover More</button>
+                <button className="btn btn-sm btn-outline btn-secondary">Latest Project</button>
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                 <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
