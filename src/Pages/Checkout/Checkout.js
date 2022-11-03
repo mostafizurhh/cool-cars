5e88f2775d813dc5a4cd9ddbfe68082e8df93e3a
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-03 22:37:11 +0000

    creted checkout form and order object

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Checkout = () => {
+    const { user } = useContext(AuthContext)
     const service = useLoaderData()
-    console.log(service)
+    // console.log(service)
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+
+        const name = form.name.value
+        const phone = form.phone.value
+        const email = form.email.value
+        const message = form.message.value
+
+        // console.log(name, phone, email, message)
+
+        const order = {
+            service: service._id,
+            serviceName: service.title,
+            price: service.price,
+            customer: name,
+            phone: phone,
+            email: email,
+            message: message
+        }
+    }
+
+
     return (
-        <div>
-            <h2>{service.title}</h2>
+        <div className='p-4 bg-rose-100 mb-16 text-center'>
+            <p className='text-2xl mb-5 text-indigo-600'>Service: {service.title}</p>
+            <p className='text-2xl mb-5 text-orange-600'>Price: ${service.price}</p>
+
+            <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center'>
+                <input name='name' type="text" placeholder="Your Name" defaultValue={user?.displayName} className="input input-bordered w-full max-w-md mb-3" readOnly />
+                <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered w-full max-w-md mb-3" required />
+                <input name='email' type="text" placeholder="Your email" defaultValue={user?.email} className="input input-bordered w-full max-w-md mb-3" readOnly />
+                <textarea name='message' type="text" placeholder="Your Message" className="textarea-bordered textarea w-full h-40 max-w-md" required />
+                <button className='btn mt-4'>Confirm Order</button>
+            </form>
         </div>
     );
 };
